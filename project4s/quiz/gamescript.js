e1b7345068ad17a0183cb6430c4dd715390c82e4
Alex Chen <43878265+alex4605@users.noreply.github.com>
2021-08-05 17:23:55 +0800

    Css change to fit zhtw

--- project4s/quiz/gamescript.js
+++ project4s/quiz/gamescript.js
@@ -57,7 +57,7 @@ function selectAnswer(e) {
   if (shuffledQuestions.length > currentQuestionIndex + 1) {
     nextButton.classList.remove('hide')
   } else {
-    startButton.innerText = 'Restart'
+    startButton.innerText = '重新再來一次'
     startButton.classList.remove('hide')
   }
 }
