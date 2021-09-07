bb109afab452872cf7cb30238827ba195d8c4348
Alex Chen <43878265+alex4605@users.noreply.github.com>
2021-09-07 17:02:54 +0800

    Update javascript/slideshow

---
+++ project4srx/navbar.js
@@ -0,0 +1,9 @@
+/*手機下拉式選單*/
+function myFunction() {
+    var x = document.getElementById("navbarhid");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
